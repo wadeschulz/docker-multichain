4578547a799989af0a1dc33e9e3197fd8f933749
Wade Schulz <wade.schulz@gmail.com>
2017-12-24 21:57:31 -0500

    add params to client node config

--- multichain/clientnode.sh
+++ multichain/clientnode.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo "rpcuser=$CHAIN_RPC_USER" > /root/.multichain/$CHAIN_NAME/multichain.conf
+echo "rpcpassword=$CHAIN_RPC_PASSWORD" >> /root/.multichain/$CHAIN_NAME/multichain.conf
+echo "rpcallowip=$CHAIN_RPC_IP" >> /root/.multichain/$CHAIN_NAME/multichain.conf
+
 multichaind $CHAIN_NAME@$CHAIN_MASTER:$CHAIN_NET_PORT
